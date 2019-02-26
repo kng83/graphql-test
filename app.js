e54ecec9561e6db7a134cbcba7b162873c6d2fc7
kng83 <pawel.kng@gmail.com>
2019-02-26 21:02:54 +0100

    some

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => {
+    console.log('here is data')
+   return 'Hello world and yuou!' 
+    
+}};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
