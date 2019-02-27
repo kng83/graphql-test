70f61835bbe03c40f6e196ea1afe268826310a8a
kng83 <pawel.kng@gmail.com>
2019-02-27 16:54:59 +0100

    som

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+let express = require('express');
+let graphqlHTTP = require('express-graphql');
+let { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
+let schema = buildSchema(`
   type Query {
     hello: String
+    whatYourName:String
   }
 `);
 
-var root = { hello: () => {
+let root = {
+  hello: () => {
     console.log('here is data')
-   return 'Hello world and yuou!' 
-    
-}};
+    return 'Hello world and yuou!'
+   },
+   whatYourName: () => {
+    return 'my name is robert'
+   }
+  }
 
-var app = express();
+
+let app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
